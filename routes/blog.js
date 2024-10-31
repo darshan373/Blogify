6c7b70c34045d477fa243150f8cfbc1d5370788b
DARSHAN KUMAR N <darshankumar372003@gmail.com>
2024-10-31 23:50:36 +0530

    "Added empty line in multer diskStorage configuration"

--- routes/blog.js
+++ routes/blog.js
@@ -8,6 +8,8 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, path.resolve("./public/uploads/"))
     },
+
+    
     filename: function (req, file, cb) {
       const filename=`${Date.now()}-${file.originalname}`
       cb(null, filename)
